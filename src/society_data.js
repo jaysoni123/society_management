c471bc607b4bb84824996a12e1f42e96f93bfbca
Dhananjay Choksi <soni.jay008@gmail.com>
2020-07-01 19:28:54 +0530

    society_management

--- src/society_data.js
+++ src/society_data.js
@@ -9,10 +9,10 @@ const societyData = [
                 name: "Sanjay V. Modi", birth_date: "25 Febuary 1974", bloodgroup: "B+", hobby: "Treavelling, Driving" ,age: 84, picture: "" 
             }},
             { name: "Naina S. Modi", data: {
-                name: "Naina S. Modi", birth_date: "25 June 1977", bloodgroup: "B+", hobby: "Reading, Painting" ,age: 84, picture: "" 
+                name: "Naina S. Modi", birth_date: "25 June 1977", bloodgroup: "O+", hobby: "Reading, Painting" ,age: 84, picture: "" 
             }},
             { name: "Ujjas S. Modi", data: {
-                name: "Ujjas S. Modi", birth_date: "14 June 1993", bloodgroup: "B+", hobby: "Swimming, Cycling, Dancing" ,age: 84, picture: "" 
+                name: "Ujjas S. Modi", birth_date: "14 June 1993", bloodgroup: "A+", hobby: "Swimming, Cycling, Dancing" ,age: 84, picture: "" 
             }}
         ]
     },
@@ -20,16 +20,16 @@ const societyData = [
         block: "block2",
         data: [
             { name: "Maheshbhai L. Patel", data: {
-                name: "Maheshbhai L. Patel", birth_date: "1 May 1968", bloodgroup: "B+", hobby: "Swimming, Cycling" ,age: 84, picture: ""
+                name: "Maheshbhai L. Patel", birth_date: "1 May 1968", bloodgroup: "A+", hobby: "Swimming, Cycling" ,age: 84, picture: ""
             }},
             { name: "Sejalben M. Patel", data: {
-                name: "Sejalben M. Patel", birth_date: "3 May 1971", bloodgroup: "B+", hobby: "Swimming, Cycling, Painting" ,age: 84, picture: "" 
+                name: "Sejalben M. Patel", birth_date: "3 May 1971", bloodgroup: "AB+", hobby: "Swimming, Cycling, Painting" ,age: 84, picture: "" 
             }},
             { name: "Kalpesh M. Patel", data: {
-                name: "Kalpesh M. Patel", birth_date: "1 July 1996", bloodgroup: "B+", hobby: "Swimming, Cycling, Painting" ,age: 84, picture: "" 
+                name: "Kalpesh M. Patel", birth_date: "1 July 1996", bloodgroup: "AB+", hobby: "Swimming, Cycling, Painting" ,age: 84, picture: "" 
             }},
             { name: "Nilesh M. Patel", data: {
-                name: "Kalpesh M. Patel", birth_date: "8 Febuary 1998", bloodgroup: "B+", hobby: "Swimming, Cycling, Painting" ,age: 84, picture: "" 
+                name: "Kalpesh M. Patel", birth_date: "8 Febuary 1998", bloodgroup: "AB+", hobby: "Swimming, Cycling, Painting" ,age: 84, picture: "" 
             }}
         ]
     },
@@ -37,10 +37,10 @@ const societyData = [
         block: "block3",
         data: [
             { name: "Vikram M. Thakor", data: {
-                name: "Vikram M. Thaklor", birth_date: "", bloodgroup: "B+", hobby: "Swimming, Cycling, Painting" ,age: 84, picture: ""
+                name: "Vikram M. Thaklor", birth_date: "", bloodgroup: "A−", hobby: "Swimming, Cycling, Painting" ,age: 84, picture: ""
             }},
             { name: "Aakansha V. Thakor", data: {
-                name: "Aakansha V. Thakor", birth_date: "", bloodgroup: "B+", hobby: "Swimming, Cycling, Painting" ,age: 84, picture: "" 
+                name: "Aakansha V. Thakor", birth_date: "", bloodgroup: "A−", hobby: "Swimming, Cycling, Painting" ,age: 84, picture: "" 
             }},
             { name: "Snehal V. Thakor", data: {
                 name: "Snehal V. Thakor", birth_date: "", bloodgroup: "B+", hobby: "Swimming, Cycling, Painting" ,age: 84, picture: "" 
@@ -60,10 +60,10 @@ const societyData = [
                 name: "Mitesh M. Mehta", birth_date: "", bloodgroup: "B+", hobby: "Swimming, Cycling, Painting" ,age: 84, picture: ""
             }},
             { name: "Nilam M. Mehata", data: {
-                name: "Nilam M. Mehata", birth_date: "", bloodgroup: "B+", hobby: "Swimming, Cycling, Painting" ,age: 84, picture: "" 
+                name: "Nilam M. Mehata", birth_date: "", bloodgroup: "B−", hobby: "Swimming, Cycling, Painting" ,age: 84, picture: "" 
             }},
             { name: "Charmi M. Mehata", data: {
-                name: "Charmi M. Mehata", birth_date: "", bloodgroup: "B+", hobby: "Swimming, Cycling, Painting" ,age: 84, picture: "" 
+                name: "Charmi M. Mehata", birth_date: "", bloodgroup: "B−", hobby: "Swimming, Cycling, Painting" ,age: 84, picture: "" 
             }},
             { name: "Chinatan M. Maheta", data: {
                 name: "Chinatan M. Maheta", birth_date: "", bloodgroup: "B+", hobby: "Swimming, Cycling, Painting" ,age: 84, picture: "" 
@@ -91,13 +91,13 @@ const societyData = [
                 name: "Vishvas C. Patel", birth_date: "", bloodgroup: "B+", hobby: "Swimming, Cycling, Painting" ,age: 84, picture: ""
             }},
             { name: "Pooja V. Patel", data: {
-                name: "Pooja V. Patel", birth_date: "", bloodgroup: "B+", hobby: "Swimming, Cycling, Painting" ,age: 84, picture: "" 
+                name: "Pooja V. Patel", birth_date: "", bloodgroup: "AB−", hobby: "Swimming, Cycling, Painting" ,age: 84, picture: "" 
             }},
             { name: "Sheetal V. Patel", data: {
                 name: "Sheetal V. Patel", birth_date: "", bloodgroup: "B+", hobby: "Swimming, Cycling, Painting" ,age: 84, picture: "" 
             }},
             { name: "Nilesh V. Patel", data: {
-                name: "Nilesh V. Patel", birth_date: "", bloodgroup: "B+", hobby: "Swimming, Cycling, Painting" ,age: 84, picture: "" 
+                name: "Nilesh V. Patel", birth_date: "", bloodgroup: "AB−", hobby: "Swimming, Cycling, Painting" ,age: 84, picture: "" 
             }}
         ]
     },
@@ -105,13 +105,13 @@ const societyData = [
         block: "block7",
         data: [
             { name: "Paresh N. Raval", data: {
-                name: "Paresh N. Raval", birth_date: "", bloodgroup: "B+", hobby: "Swimming, Cycling, Painting" ,age: 84, picture: ""
+                name: "Paresh N. Raval", birth_date: "", bloodgroup: "A+", hobby: "Swimming, Cycling, Painting" ,age: 84, picture: ""
             }},
             { name: "Kiran P. Raval", data: {
                 name: "Kiran P. Raval", birth_date: "", bloodgroup: "B+", hobby: "Swimming, Cycling, Painting" ,age: 84, picture: "" 
             }},
             { name: "Nehal p. Raval", data: {
-                name: "Nehal P. Raval", birth_date: "", bloodgroup: "B+", hobby: "Swimming, Cycling, Painting" ,age: 84, picture: "" 
+                name: "Nehal P. Raval", birth_date: "", bloodgroup: "O+", hobby: "Swimming, Cycling, Painting" ,age: 84, picture: "" 
             }},
             { name: "Jayesh P. Raval", data: {
                 name: "Jayesh P. Raval", birth_date: "", bloodgroup: "B+", hobby: "Swimming, Cycling, Painting" ,age: 84, picture: "" 
@@ -125,13 +125,13 @@ const societyData = [
                 name: "Narayanbhai M. Prajapati", birth_date: "", bloodgroup: "B+", hobby: "Swimming, Cycling, Painting" ,age: 84, picture: ""
             }},
             { name: "Sejalben N. Prajapati", data: {
-                name: "Sejalben N. Prajapati", birth_date: "", bloodgroup: "B+", hobby: "Swimming, Cycling, Painting" ,age: 84, picture: "" 
+                name: "Sejalben N. Prajapati", birth_date: "", bloodgroup: "B-", hobby: "Swimming, Cycling, Painting" ,age: 84, picture: "" 
             }},
             { name: "Karan N. Prajapati", data: {
-                name: "Karan N. Prajapati", birth_date: "", bloodgroup: "B+", hobby: "Swimming, Cycling, Painting" ,age: 84, picture: "" 
+                name: "Karan N. Prajapati", birth_date: "", bloodgroup: "AB-", hobby: "Swimming, Cycling, Painting" ,age: 84, picture: "" 
             }},
             { name: "Suraj N. Prajapati", data: {
-                name: "Suraj N. Prajapati", birth_date: "", bloodgroup: "B+", hobby: "Swimming, Cycling, Painting" ,age: 84, picture: "" 
+                name: "Suraj N. Prajapati", birth_date: "", bloodgroup: "AB-", hobby: "Swimming, Cycling, Painting" ,age: 84, picture: "" 
             }}
         ]
     },
@@ -139,19 +139,19 @@ const societyData = [
         block: "block9",
         data: [
             { name: "Karshandas A. Modi", data: {
-                name: "Karshandas A. Modi", birth_date: "", bloodgroup: "B+", hobby: "Swimming, Cycling, Painting" ,age: 84, picture: ""
+                name: "Karshandas A. Modi", birth_date: "", bloodgroup: "A-", hobby: "Swimming, Cycling, Painting" ,age: 84, picture: ""
             }},
             { name: "Kokilaben K. Modi", data: {
                 name: "Kokilaben K. Modi", birth_date: "", bloodgroup: "B+", hobby: "Swimming, Cycling, Painting" ,age: 84, picture: ""
             }},
             { name: "Milan M. Modi", data: {
-                name: "Milan M. Modi", birth_date: "", bloodgroup: "B+", hobby: "Swimming, Cycling, Painting" ,age: 84, picture: ""
+                name: "Milan M. Modi", birth_date: "", bloodgroup: "AB+", hobby: "Swimming, Cycling, Painting" ,age: 84, picture: ""
             }},
             { name: "Kinjal M. Modi", data: {
                 name: "Kinjal M. Modi", birth_date: "", bloodgroup: "B+", hobby: "Swimming, Cycling, Painting" ,age: 84, picture: "" 
             }},
             { name: "Sailesh M. Modi", data: {
-                name: "Sailesh M. Modi", birth_date: "", bloodgroup: "B+", hobby: "Swimming, Cycling, Painting" ,age: 84, picture: "" 
+                name: "Sailesh M. Modi", birth_date: "", bloodgroup: "AB+", hobby: "Swimming, Cycling, Painting" ,age: 84, picture: "" 
             }},
             { name: "Sneha M. Modi", data: {
                 name: "Sneha M. Modi", birth_date: "", bloodgroup: "B+", hobby: "Swimming, Cycling, Painting" ,age: 84, picture: "" 
@@ -182,16 +182,16 @@ const societyData = [
                 name: "Rambhai D. Patel", birth_date: "", bloodgroup: "B+", hobby: "Swimming, Cycling, Painting" ,age: 84, picture: ""
             }},
             { name: "Sitaben R. Patel", data: {
-                name: "Sitaben R. Patel", birth_date: "", bloodgroup: "B+", hobby: "Swimming, Cycling, Painting" ,age: 84, picture: "" 
+                name: "Sitaben R. Patel", birth_date: "", bloodgroup: "A+", hobby: "Swimming, Cycling, Painting" ,age: 84, picture: "" 
             }},
             { name: "Yash R. Patel", data: {
-                name: "Yash R. Patel", birth_date: "", bloodgroup: "B+", hobby: "Swimming, Cycling, Painting" ,age: 84, picture: "" 
+                name: "Yash R. Patel", birth_date: "", bloodgroup: "AB+", hobby: "Swimming, Cycling, Painting" ,age: 84, picture: "" 
             }},
             { name: "Minal R. Patel", data: {
-                name: "Minal R. Patel", birth_date: "", bloodgroup: "B+", hobby: "Swimming, Cycling, Painting" ,age: 84, picture: "" 
+                name: "Minal R. Patel", birth_date: "", bloodgroup: "AB+", hobby: "Swimming, Cycling, Painting" ,age: 84, picture: "" 
             }},
             { name: "Kiran R. Patel", data: {
-                name: "Kiran R. Patel", birth_date: "", bloodgroup: "B+", hobby: "Swimming, Cycling, Painting" ,age: 84, picture: "" 
+                name: "Kiran R. Patel", birth_date: "", bloodgroup: "AB+", hobby: "Swimming, Cycling, Painting" ,age: 84, picture: "" 
             }}
         ]
     },
@@ -199,19 +199,19 @@ const societyData = [
         block: "block12",
         data: [
             { name: "Jayentibhai G. Solanki", data: {
-                name: "Jayentibhai G. Solanki", birth_date: "", bloodgroup: "B+", hobby: "Swimming, Cycling, Painting" ,age: 84, picture: ""
+                name: "Jayentibhai G. Solanki", birth_date: "", bloodgroup: "AB+", hobby: "Swimming, Cycling, Painting" ,age: 84, picture: ""
             }},
             { name: "Minaben J. Solanki", data: {
-                name: "Minaben J. Solanki", birth_date: "", bloodgroup: "B+", hobby: "Swimming, Cycling, Painting" ,age: 84, picture: "" 
+                name: "Minaben J. Solanki", birth_date: "", bloodgroup: "B-", hobby: "Swimming, Cycling, Painting" ,age: 84, picture: "" 
             }},
             { name: "Axay J. Solanki", data: {
                 name: "Axay J. Solanki", birth_date: "", bloodgroup: "B+", hobby: "Swimming, Cycling, Painting" ,age: 84, picture: "" 
             }},
             { name: "Smita J. Patel", data: {
-                name: "Smita J. Patel", birth_date: "", bloodgroup: "B+", hobby: "Swimming, Cycling, Painting" ,age: 84, picture: "" 
+                name: "Smita J. Patel", birth_date: "", bloodgroup: "A+", hobby: "Swimming, Cycling, Painting" ,age: 84, picture: "" 
             }},
             { name: "Shilpa J. Patel", data: {
-                name: "Shilpa J. Patel", birth_date: "", bloodgroup: "B+", hobby: "Swimming, Cycling, Painting" ,age: 84, picture: "" 
+                name: "Shilpa J. Patel", birth_date: "", bloodgroup: "O+", hobby: "Swimming, Cycling, Painting" ,age: 84, picture: "" 
             }}
         ]
     },
@@ -222,10 +222,10 @@ const societyData = [
                 name: "Rajukumar B. Kothari", birth_date: "", bloodgroup: "B+", hobby: "Swimming, Cycling, Painting" ,age: 84, picture: ""
             }},
             { name: "Nehaben R. Kothari", data: {
-                name: "Nehaben R. Kothari", birth_date: "", bloodgroup: "B+", hobby: "Swimming, Cycling, Painting" ,age: 84, picture: "" 
+                name: "Nehaben R. Kothari", birth_date: "", bloodgroup: "A+", hobby: "Swimming, Cycling, Painting" ,age: 84, picture: "" 
             }},
             { name: "Arjun R. Kothari", data: {
-                name: "Arjun R. Kothari", birth_date: "", bloodgroup: "B+", hobby: "Swimming, Cycling, Painting" ,age: 84, picture: "" 
+                name: "Arjun R. Kothari", birth_date: "", bloodgroup: "O+", hobby: "Swimming, Cycling, Painting" ,age: 84, picture: "" 
             }},
             { name: "Parth R. kothari", data: {
                 name: "Parth R. kothari", birth_date: "", bloodgroup: "B+", hobby: "Swimming, Cycling, Painting" ,age: 84, picture: "" 
@@ -236,7 +236,7 @@ const societyData = [
         block: "block14",
         data: [
             { name: "Nileshbhai A. Patel", data: {
-                name: "Nileshbhai A. Patel", birth_date: "", bloodgroup: "B+", hobby: "Swimming, Cycling, Painting" ,age: 84, picture: ""
+                name: "Nileshbhai A. Patel", birth_date: "", bloodgroup: "A-", hobby: "Swimming, Cycling, Painting" ,age: 84, picture: ""
             }},
             { name: "Mineshaben N. Patel", data: {
                 name: "Mineshaben N. Patel", birth_date: "", bloodgroup: "B+", hobby: "Swimming, Cycling, Painting" ,age: 84, picture: "" 
@@ -245,7 +245,7 @@ const societyData = [
                 name: "Smit N. Patel", birth_date: "", bloodgroup: "B+", hobby: "Swimming, Cycling, Painting" ,age: 84, picture: "" 
             }},
             { name: "Aarju N. Patel", data: {
-                name: "Aarju N. Patel", birth_date: "", bloodgroup: "B+", hobby: "Swimming, Cycling, Painting" ,age: 84, picture: "" 
+                name: "Aarju N. Patel", birth_date: "", bloodgroup: "AB-", hobby: "Swimming, Cycling, Painting" ,age: 84, picture: "" 
             }}
         ]
     },
@@ -253,7 +253,7 @@ const societyData = [
         block: "block15",
         data: [
             { name: "Pinkesh D. Modi", data: {
-                name: "Pinkesh D. Modi", birth_date: "", bloodgroup: "B+", hobby: "Swimming, Cycling, Painting" ,age: 84, picture: ""
+                name: "Pinkesh D. Modi", birth_date: "", bloodgroup: "A+", hobby: "Swimming, Cycling, Painting" ,age: 84, picture: ""
             }},
             { name: "Sunena P. Modi", data: {
                 name: "Sunena P. Modi", birth_date: "", bloodgroup: "B+", hobby: "Swimming, Cycling, Painting" ,age: 84, picture: "" 
@@ -262,7 +262,7 @@ const societyData = [
                 name: "Harsh P. Modi", birth_date: "", bloodgroup: "B+", hobby: "Swimming, Cycling, Painting" ,age: 84, picture: "" 
             }},
             { name: "Priyansi P. Modi", data: {
-                name: "Priyansi P. Modi", birth_date: "", bloodgroup: "B+", hobby: "Swimming, Cycling, Painting" ,age: 84, picture: "" 
+                name: "Priyansi P. Modi", birth_date: "", bloodgroup: "AB+", hobby: "Swimming, Cycling, Painting" ,age: 84, picture: "" 
             }}
         ]
     }
